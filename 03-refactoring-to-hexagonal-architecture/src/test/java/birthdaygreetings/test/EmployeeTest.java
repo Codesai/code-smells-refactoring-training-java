f030d821876fb58f5b0c267a85ce1fda6298f375
freyes <fran@codesai.com>
2024-11-20 15:14:04 +0000

    fix tests

--- 03-refactoring-to-hexagonal-architecture/src/test/java/birthdaygreetings/test/EmployeeTest.java
+++ 03-refactoring-to-hexagonal-architecture/src/test/java/birthdaygreetings/test/EmployeeTest.java
@@ -17,19 +17,4 @@ public class EmployeeTest {
         assertTrue(employee.isBirthday(new OurDate("2008/01/31")),
                 "his birthday");
     }
-
-    @Test
-    public void equality() throws Exception {
-        Employee base = new Employee("First", "Last", "1999/09/01",
-                "first@last.com");
-        Employee same = new Employee("First", "Last", "1999/09/01",
-                "first@last.com");
-        Employee different = new Employee("First", "Last", "1999/09/01",
-                "boom@boom.com");
-
-        assertFalse(base.equals(null));
-        assertFalse(base.equals(""));
-        assertTrue(base.equals(same));
-        assertFalse(base.equals(different));
-    }
 }
--- 03-refactoring-to-hexagonal-architecture/src/test/java/birthdaygreetings/test/OurDateTest.java
+++ 03-refactoring-to-hexagonal-architecture/src/test/java/birthdaygreetings/test/OurDateTest.java
@@ -7,34 +7,17 @@ import birthdaygreetings.OurDate;
 import org.junit.jupiter.api.Test;
 
 public class OurDateTest {
-    @Test
-    public void getters() throws Exception {
-        OurDate ourDate = new OurDate("1789/01/24");
-        assertEquals(1, ourDate.getMonth());
-        assertEquals(24, ourDate.getDay());
-    }
 
     @Test
     public void isSameDate() throws Exception {
         OurDate ourDate = new OurDate("1789/01/24");
         OurDate sameDay = new OurDate("2001/01/24");
         OurDate notSameDay = new OurDate("1789/01/25");
-        OurDate notSameMonth = new OurDate("1789/02/25");
+        OurDate notSameMonth = new OurDate("1789/02/24");
 
         assertTrue(ourDate.isSameDay(sameDay), "same");
         assertFalse(ourDate.isSameDay(notSameDay), "not same day");
         assertFalse(ourDate.isSameDay(notSameMonth), "not same month");
     }
 
-    @Test
-    public void equality() throws Exception {
-        OurDate base = new OurDate("2000/01/02");
-        OurDate same = new OurDate("2000/01/02");
-        OurDate different = new OurDate("2000/01/04");
-
-        assertNotEquals(null, base);
-        assertEquals(base, base);
-        assertEquals(base, same);
-        assertNotEquals(base, different);
-    }
 }
